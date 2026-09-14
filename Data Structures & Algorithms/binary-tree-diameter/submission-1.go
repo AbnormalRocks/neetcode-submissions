82586ef859e182e137601ab053d1f4b2bf701bb2
Swarnim <35725054+AbnormalRocks@users.noreply.github.com>
2026-09-14 18:24:48 +0530

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	m:=0
+    rec(root,&m)
+	return m
+}
+func rec(t *TreeNode,m *int)int{
+	if t==nil{
+		return 0
+	}
+	lh:=rec(t.Left,m)
+	rh:=rec(t.Right,m)
+	*m=max(*m,lh+rh)
+	h:=1+max(lh,rh)
+	return h
+}
